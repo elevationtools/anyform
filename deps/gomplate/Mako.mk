c51233aaf25bd12bd6cb49b5bd32dec7123db50c
Brian Braunstein <git@bristyle.com>
2024-04-01 12:40:40 +0200

    Add gomplate to //deps.

---
+++ deps/gomplate/Mako.mk
@@ -0,0 +1,20 @@
+
+include $(MAKO_ROOT)/dep.mk
+
+define DEPS
+	$(REPO_ROOT)/deps/golang
+endef
+
+ifeq "$(MAKO_STAGE)" "main"
+
+Linux-x86_64:
+	mkdir -p genfiles
+	curl -Lo genfiles/download \
+		'https://github.com/hairyhenderson/gomplate/releases/download/v4.0.0-pre-2/gomplate_linux-amd64'
+	chmod +x genfiles/download
+
+smoketest:
+	gomplate --version | grep 4.0.0-pre-2
+
+endif
+
